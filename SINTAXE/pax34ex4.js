ad871467d3907144a623ad7d94fe029bd23823d5
P3R0L <avazquezperol@gmail.com>
2025-05-12 14:02:11 +0200

    añadido ejercicio SINTAXE (pax34ex4.html, pax34ex4.js)

---
+++ SINTAXE/pax34ex4.js
@@ -0,0 +1,5 @@
+// Escribe as potencias de 2, dende 2⁰ ata 2²⁰. Para cada potencia debe saír un texto similar a “2 elevado a 0 = 1”.
+for (let i = 0; i < 21; i++) {
+  potencia = 2 ** i;
+  console.log("2 elevado a " + i + " = " + potencia);
+}
