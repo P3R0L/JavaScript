d2d6e01bd97d77fa8d00d0d42c53b74da1432dbf
P3R0L <avazquezperol@gmail.com>
2025-05-14 11:37:40 +0200

    añadido ejercicio OBXECTOS (pax4ex3.html, pax4ex3.js)

---
+++ OBXECTOS/pax4ex3.js
@@ -0,0 +1,11 @@
+//Crea unha función á que se lle pase unha cadea de números e devolva unha cadea da mesma lonxitude formada por * e as últimas 4 cifras do parámetro de entrada.
+function enmascarar(cadea) {
+  let novaCadea = "";
+  for (let i = 0; i < cadea.length - 4; i++) {
+    novaCadea = novaCadea + "*";
+  }
+  novaCadea = novaCadea + cadea.substring(cadea.length - 4, cadea.length);
+  return novaCadea;
+}
+
+console.log(enmascarar("1234123412347777")); // ************7777
