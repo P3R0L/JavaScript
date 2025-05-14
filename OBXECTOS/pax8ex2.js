625d6bd8347c87cc530b099568a858a4bc31063a
P3R0L <avazquezperol@gmail.com>
2025-05-14 13:49:34 +0200

    modificado ejercicio OBXECTOS (pax8ex2.js)

--- OBXECTOS/pax8ex2.js
+++ OBXECTOS/pax8ex2.js
@@ -1,31 +1,16 @@
 // Crea unha función á que se lle pase un mes (1-12) e un ano e devolva o número de
 // días dese mes.
 function calcularNumeroDias(mes, ano) {
-  let mesCalculado = mes - 1;
-  let dias;
-  if (
-    mesCalculado === 0 ||
-    mesCalculado === 2 ||
-    mesCalculado === 4 ||
-    mesCalculado === 6 ||
-    mesCalculado === 7 ||
-    mesCalculado === 9 ||
-    mesCalculado === 11
-  ) {
-    dias = 31;
-    return dias;
+  const mesesCon31 = [0, 2, 4, 6, 7, 9, 11]; // Índices de enero, marzo, etc.
+  const mesCalculado = mes - 1;
+
+  if (mesesCon31.includes(mesCalculado)) {
+    return 31;
   } else if (mesCalculado === 1) {
-    if ((ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0) {
-      dias = 29;
-      return dias;
-    } else {
-      dias = 28;
-      return dias;
-    }
+    return (ano % 4 === 0 && ano % 100 !== 0) || ano % 400 === 0 ? 29 : 28;
   } else {
-    dias = 30;
-    return dias;
+    return 30;
   }
 }
 
-console.log(calcularNumeroDias(2, 2024));
+console.log(calcularNumeroDias(2, 2025));
