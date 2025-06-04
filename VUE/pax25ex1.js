bf4b19cf7fb9a6afb146e5f63a11b0398b6d634d
P3R0L <avazquezperol@gmail.com>
2025-06-04 20:03:37 +0200

    añadido ejercicio VUE (pax25ex1.html, pax25ex1.js)

---
+++ VUE/pax25ex1.js
@@ -0,0 +1,27 @@
+// Crea un formulario con, polo menos, unha caixa de texto, un área de texto, múltiples
+// checkboxes, radiobuttons e select. Fai que o valor seleccionado en cada un dos
+// campos se mostre en HTML.
+// Engade un botón para resetear o formulario, de forma que os valores
+// escritos/seleccionados se borren.
+import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp({
+  data() {
+    return {
+      textoInput: "",
+      textoTextArea: "",
+      checkedCheckboxes: [],
+      checkedRadioButton: "",
+      selectedOption: "",
+    };
+  },
+  methods: {
+    resetearValores() {
+      this.textoInput = "";
+      this.textoTextArea = "";
+      this.checkedCheckboxes = [];
+      this.checkedRadioButton = "";
+      this.selectedOption = "";
+    },
+  },
+}).mount("#app");
