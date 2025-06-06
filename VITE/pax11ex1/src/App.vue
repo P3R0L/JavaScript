1f0fa8290e204c5ad1491b1009a9b624610f76e3
P3R0L <avazquezperol@gmail.com>
2025-06-06 20:05:51 +0200

    añadido proyecto VITE (pax11ex1)

---
+++ VITE/pax11ex1/src/App.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      mensaxe: "ola mundo",
+    };
+  },
+  methods: {
+    toggle() {
+      this.mensaxe == this.mensaxe.toUpperCase()
+        ? (this.mensaxe = this.mensaxe.toLowerCase())
+        : (this.mensaxe = this.mensaxe.toUpperCase());
+    },
+    engadirExclamacion() {
+      this.mensaxe = this.mensaxe + "!";
+    },
+  },
+};
+</script>
+
+<template>
+  <p>{{ mensaxe }}</p>
+  <button @click="toggle()">Toggle</button>
+  <button @click="engadirExclamacion()">!</button>
+</template>
+
+<style></style>
