02a6402766f4333ae6e612f8a38be5b237aed0ef
P3R0L <avazquezperol@gmail.com>
2025-06-06 20:25:57 +0200

    modificado proyecto VITE (pax11ex2)

--- VITE/pax11ex2/src/App.vue
+++ VITE/pax11ex2/src/App.vue
@@ -1,47 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+// Crea un proxecto Vue con un compoñente a reutilizar que conteña unha propiedade
+// que almacene o número de veces que se pulsou nun botón. Engade ao compoñente
+// un botón que incremente o contador anterior en unha unidade cada vez que se
+// pulse. O texto que aparece no botón debe informar do número de veces se pulsou.
+// Engade no compoñente raíz da aplicación 3 instancias do compoñente anterior. ¿O
+// valor do contador é compartido polos compoñentes ? Non, é independente
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <Contador /><br />
+  <Contador /><br />
+  <Contador />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
---
+++ VITE/pax11ex2/src/components/Contador.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  computed: {
+    incrementarContador() {
+      this.contador++;
+    },
+  },
+};
+</script>
+
+<template>
+  <button @click="incrementarContador()">
+    Vostede pulsou {{ contador }} este botón
+  </button>
+</template>
+
+<style></style>
