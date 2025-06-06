fa0f12abc90597cbe43024c2c34b32d069040f58
P3R0L <avazquezperol@gmail.com>
2025-06-06 20:06:46 +0200

    modificado proyecto VITE (pax11ex1)

--- VITE/pax11ex1/src/App.vue
+++ VITE/pax11ex1/src/App.vue
@@ -1,4 +1,10 @@
 <script>
+// Crea un proxecto Vue onde o compoñente raíz da aplicación teña unha propiedade
+// que sexa unha mensaxe de texto, por exemplo “ola mundo”. Configura o código
+// HTML para mostrar a mensaxe. Engade ao HTML un botón de tal forma que cada
+// vez que se pulse transforme a mensaxe de minúscula a maiúscula ou viceversa.
+// Engade outro botón que permita engadir un carácter, por exemplo “!” á mensaxe de
+// texto.
 export default {
   data() {
     return {
