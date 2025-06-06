1218962b1e203bc6c3391fea5e1ca634b5b14178
P3R0L <avazquezperol@gmail.com>
2025-06-06 20:43:33 +0200

    añadido proyecto VITE (pax11ex3)

---
+++ VITE/pax11ex3/src/App.vue
@@ -0,0 +1,19 @@
+<script>
+// Crea un proxecto Vue para mostrar unha lista de contactos. A información dos
+// contactos debe estar nun compoñente separado (Contact) no que se debe gardar a
+// información dunha persoa: nome, teléfono e correo electrónico, como mínimo.
+// De momento, inicializa manualmente os datos persoais dunha soa persoa no
+// compoñente Contact.
+// Engade dúas instancias do compoñente Contact na compoñente raíz da aplicación.
+// Configura o compoñente Contact para que mostre o nome da persoa e teña un
+// botón para mostrar/ocultar o resto da información.
+// Cando se lance a aplicación debe mostrar en pantalla os seguintes datos. Observar
+// que o botón permite mostrar/ocultar os detalles.
+</script>
+
+<template>
+  <Contact />
+  <Contact />
+</template>
+
+<style></style>
---
+++ VITE/pax11ex3/src/components/Contact.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      contacto: {
+        nome: "Ada Lovelace",
+        tlf: "0123 45678 90",
+        correo: "ada@localhost.com",
+      },
+      visible: false,
+    };
+  },
+  methods: {
+    mostrarDetalles() {
+      this.visible = !this.visible;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Listado</h1>
+  <h1>{{ contacto.nome }}</h1>
+  <button @click="mostrarDetalles()">Mostrar detalles</button>
+  <template v-if="visible">
+    <p>Teléfono: {{ contacto.tlf }}</p>
+    <p>Correo: {{ contacto.correo }}</p>
+  </template>
+</template>
+
+<style></style>
