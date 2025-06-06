ad556dc68a2e7bc34bcefd8b5b8b92b2bf5442a0
P3R0L <avazquezperol@gmail.com>
2025-06-06 21:31:21 +0200

    añadido proyecto VITE (pax17ex2)

---
+++ VITE/pax17ex2/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+// Modifica o exercicio do listado de contactos do apartado anterior para que o array de
+// persoas estea definido no compoñente pai e se pase a información ao compoñente
+// fillo utilizando props. No compoñente raíz da aplicación debes crear un array con
+// dúas persoas mínimo indicando o nome, teléfono e correo electrónico.
+// Crea unha aplicación que mostre a información dos contactos utilizando o
+// compoñente Contact.
+// Implementa a validacion de props.
+export default {
+  data() {
+    return {
+      persoas: [
+        {
+          nome: "Ada Lovelace",
+          tlf: "0123 45678 90",
+          correo: "ada@localhost.com",
+        },
+        {
+          nome: "Grace Hopper",
+          tlf: "1234 56789 01",
+          correo: "grace@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <Contact v-bind="persoas[0]" />
+  <Contact
+    :nome="persoas[1].nome"
+    :tlf="persoas[1].tlf"
+    :correo="persoas[1].correo"
+  />
+</template>
+
+<style></style>
---
+++ VITE/pax17ex2/src/components/Contact.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  props: {
+    nome: String,
+    tlf: String,
+    correo: String,
+  },
+  data() {
+    return {
+      visible: false,
+    };
+  },
+  methods: {
+    mostrarDetalles() {
+      this.visible = !this.visible;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Listado</h1>
+  <h1>{{ nome }}</h1>
+  <button @click="mostrarDetalles()">Mostrar detalles</button>
+  <template v-if="visible">
+    <p>Teléfono: {{ tlf }}</p>
+    <p>Correo: {{ correo }}</p>
+  </template>
+</template>
+
+<style></style>
