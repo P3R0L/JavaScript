89f9e0d0564d0153d98ff4b4e839cf8959fe7a89
P3R0L <avazquezperol@gmail.com>
2025-06-06 21:59:48 +0200

    añadido proyecto VITE (pax17ex3)

---
+++ VITE/pax17ex3/src/App.vue
@@ -0,0 +1,43 @@
+<script>
+// Engade ao exercicio anterior información de se un contacto é favorito ou non. Esta
+// información almacénase no array do compoñente raíz e pásase ao compoñente
+// Contact como prop.
+// Mostra visualmente se é un contacto favorito ou non. Podes utilizar texto ou imaxes,
+// como se mostra a continuacion.
+// Engade tamén ao compoñente descendente a funcionalidade que permita modificar
+// se un compoñente é favorito ou non. Fai que visualmente se actualice a información
+// de se é ou non favorito. De momento non se explicou como cambiar este dato no
+// compoñente ascendente.
+export default {
+  data() {
+    return {
+      persoas: [
+        {
+          nome: "Ada Lovelace",
+          tlf: "0123 45678 90",
+          correo: "ada@localhost.com",
+          favorito: true,
+        },
+        {
+          nome: "Grace Hopper",
+          tlf: "1234 56789 01",
+          correo: "grace@localhost.com",
+          favorito: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <Contact v-bind="persoas[0]" />
+  <Contact
+    :nome="persoas[1].nome"
+    :tlf="persoas[1].tlf"
+    :correo="persoas[1].correo"
+    :favorito="persoas[1].favorito"
+  />
+</template>
+
+<style></style>
---
+++ VITE/pax17ex3/src/components/Contact.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    nome: String,
+    tlf: String,
+    correo: String,
+    favorito: Boolean,
+  },
+  data() {
+    return {
+      visible: false,
+      isFavorito: this.favorito,
+    };
+  },
+  methods: {
+    mostrarDetalles() {
+      this.visible = !this.visible;
+    },
+    alternarFavorito() {
+      this.isFavorito = !this.isFavorito;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Listado</h1>
+  <h1>
+    {{ nome }}
+    <span v-if="isFavorito" @click="alternarFavorito()">(Favorito) ★</span>
+    <span v-else @click="alternarFavorito()">☆</span>
+  </h1>
+
+  <button @click="mostrarDetalles()">Mostrar detalles</button>
+  <template v-if="visible">
+    <p>Teléfono: {{ tlf }}</p>
+    <p>Correo: {{ correo }}</p>
+  </template>
+</template>
+
+<style></style>
